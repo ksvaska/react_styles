7171dc20cda73025a3d49e10aeedc5dca4459391
ksvaska <vas25ks@gmail.com>
2024-03-30 17:37:30 +0300

    k

--- react_styles/src/App.js
+++ react_styles/src/App.js
@@ -1,29 +1,46 @@
 
 import './App.css';
-import Card from './components/card.jsx'
+import Card from './components/card.jsx';
+import themeBlue from "./components/themeBlue.module.css";
+import themeRed from "./components/themeRed.module.css";
+import themeGreen from './components/themeGreen.module.css';
+import themeBlack from './components/themeBlack.module.css';
+
 
 
 function App() {
+
   return (
     <div className="App">
       <Card
       price = '300'
       speed = '10'
+      theme={themeBlue}
+      selected={false}
+
       />
       <Card
        price = '450'
        speed = '50'
+       theme={themeGreen}
+       selected={false}
+     
       />
       <Card
-       price = '550'
+       price= '550'
        speed = '100'
+       theme={themeRed}
+       selected={true}
       />
       <Card
        price = '1000'
        speed = '200'
+       theme={themeBlack}
+       selected={false}
       />
     </div>
   );
+
 }
 
 export default App;
--- react_styles/src/components/card.jsx
+++ react_styles/src/components/card.jsx
@@ -1,13 +1,19 @@
 import React from "react";
+import styles from "./card.module.css";
 
-function Card(props){
-    const {price,speed} = props;
+function Card({price,speed,theme,selected}){
+    
+
     return(
-        <div className="card-body">
-            <p className="card-title">Безлимитный {price}</p>
-            <p className="card-price">руб {price}/мес</p>
-            <p className="card-speed"> до {speed} Мбит/сек</p>
-            <p>Объем включенного трафика не ограничен</p>
+        <div className={{selected}? styles.selected : styles.card   }>
+            <p className={`${styles.title} ${theme.title}`}>Безлимитный {price}</p>
+            <div className={`${styles.info} ${theme.price}`}>
+                <p className={styles.rub}>руб</p>
+                <p className={styles.price}>{price}</p>
+                <p className={styles.month}>/мес</p>
+            </div>
+            <p className={styles.speed}> до {speed} Мбит/сек</p>
+            <p className={styles.footer}>Объем включенного  <br/> трафика не ограничен</p>
         </div>
 
     );
